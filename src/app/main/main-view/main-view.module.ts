8337fa12ed1c50351817fa3ba58c329b92e11cc7
TheVoidek <wojtek.piechaczek@gmail.com>
2022-04-23 20:57:06 +0200

    Basic filterbar behavior

--- src/app/main/main-view/main-view.module.ts
+++ src/app/main/main-view/main-view.module.ts
@@ -6,8 +6,10 @@ import {MatSelectModule} from "@angular/material/select";
 import {MatSlideToggleModule} from "@angular/material/slide-toggle";
 import {MatRadioModule} from "@angular/material/radio";
 import {MatInputModule} from "@angular/material/input";
+import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from "@angular/material/form-field";
 import {MatDatepickerModule} from "@angular/material/datepicker";
+import {MatChipsModule} from '@angular/material/chips'; 
 import {MatCheckboxModule} from "@angular/material/checkbox";
 import {MatButtonModule} from "@angular/material/button";
 import {MatAutocompleteModule} from "@angular/material/autocomplete";
@@ -17,19 +19,24 @@ import {MainViewComponent} from "./main-view.component";
 import {HttpClientModule} from "@angular/common/http";
 import {DragDropModule} from "@angular/cdk/drag-drop";
 import {HephaestusTableComponent} from "../../shared/components/table/hephaestus-table/hephaestus-table.component";
+import { SearchFilterComponent } from 'src/app/shared/components/search-filter/search-filter/search-filter.component';
+import { ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     MainViewComponent,
-    HephaestusTableComponent
+    HephaestusTableComponent,
+    SearchFilterComponent,
   ],
   imports: [
     BrowserAnimationsModule,
     MatAutocompleteModule,
     MatButtonModule,
     MatCheckboxModule,
+    MatChipsModule,
     MatDatepickerModule,
     MatFormFieldModule,
+    MatIconModule,
     MatInputModule,
     MatRadioModule,
     MatSelectModule,
@@ -37,7 +44,8 @@ import {HephaestusTableComponent} from "../../shared/components/table/hephaestus
     MatSlideToggleModule,
     DragDropModule,
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    ReactiveFormsModule,
   ],
   providers: [HephaestusService],
   exports: [
