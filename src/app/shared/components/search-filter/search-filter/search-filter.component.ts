d2b13809f641644820241c6392aff6f37cf83981
TheVoidek <wojtek.piechaczek@gmail.com>
2022-04-23 11:41:08 +0200

    implemented getting metadata in prometheus service

--- src/app/shared/components/search-filter/search-filter/search-filter.component.ts
+++ src/app/shared/components/search-filter/search-filter/search-filter.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PrometheusService } from 'src/app/shared/service/prometheus/prometheus.service';
 
 @Component({
   selector: 'app-search-filter',
@@ -7,9 +8,19 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SearchFilterComponent implements OnInit {
 
-  constructor() { }
+  constructor(private prometheusService: PrometheusService) { }
 
   ngOnInit(): void {
+    this.prometheusService.getLabels().subscribe((labels) => {
+      //TODO remove this, temporary log
+      console.log("All labels: ");
+      console.log(labels);
+    });
+    //TODO remove this, temporary log
+    this.prometheusService.getLabelValues("__name__").subscribe((values) => {
+      console.log("All names: ");
+      console.log(values);
+    });
   }
 
 }
--- src/app/shared/service/prometheus/prometheus.service.ts
+++ src/app/shared/service/prometheus/prometheus.service.ts
@@ -1,9 +1,32 @@
 import { Injectable } from '@angular/core';
+import {BaseService} from "../base-service";
+import {HttpClient} from "@angular/common/http";
+import { Observable, mergeMap } from 'rxjs';
+import { ListResponse } from '../../models/reponse/ListResponse.model';
 
 @Injectable({
   providedIn: 'root'
 })
-export class PrometheusService {
+export class PrometheusService extends BaseService {
 
-  constructor() { }
+  private readonly addressUrl: string = "/prometheus/address";
+  private readonly prometheusUrl: Observable<string>;
+
+  constructor(http: HttpClient) {
+    super(http);
+    this.prometheusUrl = this.get(this.addressUrl);
+  }
+
+  public getLabels(): Observable<ListResponse> {
+    return this.prometheusUrl.pipe(mergeMap( 
+      (url) => this.http.get<ListResponse>(url + "/api/v1/labels")
+    ));
+  }
+
+  public getLabelValues(label: string): Observable<ListResponse> {
+    return this.prometheusUrl.pipe(mergeMap( 
+      (url) => this.http.get<ListResponse>(url + "/api/v1/label/" + label + "/values")
+    ));
+  }
+
 }
