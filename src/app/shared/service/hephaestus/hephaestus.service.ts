be87476b6e7067b6a99a15730581e33f665e9c8f
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2022-05-23 00:49:01 +0200

    added query to backed regarding saved metrics

--- src/app/shared/service/hephaestus/hephaestus.service.ts
+++ src/app/shared/service/hephaestus/hephaestus.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import {BaseService} from "../base-service";
 import {HttpClient} from "@angular/common/http";
+import { Filters } from '../../models/metrics/filters.model';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -9,13 +11,21 @@ export class HephaestusService extends BaseService {
 
   private saveMetricUrl: string = '/hephaestus/metrics/save';
 
+  //todo change to actual link from config
+  private savedMetricUrl: string = 'http://localhost:8080/hephaestus/metrics/saved/';
+
   constructor(http: HttpClient) {
     super(http);
   }
 
   public saveMetrics(metrics: {}) { // todo add type
-    //todo REFACTOR !!! KS
+    //todo REFACTOR !!! KS]
+    console.log(metrics)
     return this.put(this.saveMetricUrl, metrics);
+  }
+
+  public getSavedMetrics(): Observable<any[]>{
+    return this.http.get<any[]>(this.savedMetricUrl);
   }
 
 }
