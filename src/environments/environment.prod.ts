a1c50c6847b4e61ceec9b446a5d1b6c4d23a467b
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2022-05-24 14:40:56 +0200

    semi- wroking deployment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,5 @@
 export const environment = {
-  production: true
+  production: true,
+  backendUrl: 'http://hephaestus-gui-backend:8080',
+  metricsAdapterUrl: 'http://localhost:8081>>>TO-CHANGE<<<'
 };
