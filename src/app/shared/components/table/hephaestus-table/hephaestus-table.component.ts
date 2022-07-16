b60ff7508095cb59c3037b1db6a4ca1128f89594
Michał Szczurek <73036080+Ra77us@users.noreply.github.com>
2022-07-16 21:00:40 +0200

    Added Pagination without styling

--- src/app/shared/components/table/hephaestus-table/hephaestus-table.component.ts
+++ src/app/shared/components/table/hephaestus-table/hephaestus-table.component.ts
@@ -5,11 +5,9 @@ import { HephaestusService } from "../../../service/hephaestus/hephaestus.servic
 import { toMetricItem } from "./items/ToMetricItem";
 import { PrometheusService } from 'src/app/shared/service/prometheus/prometheus.service';
 import { DataProvider } from "../../../service/data-provider";
-import { ElementRef } from '@angular/core';
 import { CdkScrollable } from '@angular/cdk/scrolling';
 import { MetricsAdapterService } from "../../../service/metrics-adapter/metrics-adapter.service";
-import { Filters } from 'src/app/shared/models/metrics/filters.model';
-import { take } from 'rxjs';
+import {PageEvent} from '@angular/material/paginator';
 
 @Component({
   selector: 'app-hephaestus-table',
@@ -22,51 +20,70 @@ export class HephaestusTableComponent implements OnInit {
   public selectedMetrics: MetricItem[] = [];
   private filterMatchingMetrics: MetricItem[] = [];
   public availableMetrics: MetricItem[] = [];
-  @ViewChild('leftTableScrollbar', { read: CdkScrollable })
-  private leftScrollBar: CdkScrollable = {} as CdkScrollable;
+  @ViewChild('leftTableScrollbar', {read: CdkScrollable}) 
+  private leftScrollBar: CdkScrollable =  {} as CdkScrollable;
+  public availableMetricsPageIndex: number = 0;
+  public availableMetricsPageSize: number = 10;
+  public selectedMetricsPageIndex: number = 0;
+  public selectedMetricsPageSize: number = 10;
 
   constructor(
-    private hephaestusService: HephaestusService,
-    private prometheusService: PrometheusService,
-    private dataProvider: DataProvider,
-    ) { }
+      private hephaestusService: HephaestusService,
+      private prometheusService: PrometheusService,
+      private dataProvider: DataProvider,
+      private metricsAdapterService: MetricsAdapterService) {}
 
   ngOnInit(): void {
     this.getMetrics();
-    this.loadSavedMetrics();
+    for (let i = 1; i < 1000; i++){
+      this.availableMetrics.push(new MetricItem(new Map([[i.toString(), "b"], ["a1", "b2"], ["a3", "b"], ["a4", "b"]]), false));
+    }
+    this.availableMetrics.forEach(x => this.filterMatchingMetrics.push(x));
   }
 
-  loadSavedMetrics(): void{
-    const data = this.hephaestusService.getSavedMetrics().pipe(take(1)).subscribe((savedFilters: any[]) => {
-      for (const metric of savedFilters) {
-        const labels: Map<string, string> = new Map();
-        for (const val in metric.values) {
-          labels.set(val, metric.values[val]);
-        }
-        const newMetric: MetricItem = new MetricItem(labels, metric.isQuery);
-        for (const metric of this.selectedMetrics) {
-          metric.checkConflict(newMetric);
-        }
-        this.selectedLabelsSet.add(JSON.stringify(Array.from(newMetric.labels.entries())));
-        this.selectedMetrics.push(newMetric);
-      }
-    });
+  itemsInRange(min: number, max: number, source: any[]){
+    let res: any[] = [];
+    for (let i = min; i < Math.min(max, source.length); i++){
+      res.push(source[i]);
+    }
+    return res;
   }
 
-  drop(event: CdkDragDrop<MetricItem[]>) {
+ 
+  // todo: if sb knows how to pass number by ref in ts 2 functions below could be merged
+  changeAvailableMetricsPage(event: PageEvent){
+    this.availableMetricsPageIndex = event.pageIndex;
+    if (this.availableMetricsPageSize !== event.pageSize){
+      this.availableMetricsPageSize  = event.pageSize;
+      // current strategy for size change - go to the beginning of the list
+      this.availableMetricsPageIndex = 0;
+    }
+ }
+
+ changeSelectedMetricsPage(event: PageEvent){
+  this.selectedMetricsPageIndex = event.pageIndex;
+  if (this.selectedMetricsPageSize !== event.pageSize){
+    this.selectedMetricsPageSize  = event.pageSize;
+    // current strategy for size change - go to the beginning of the list
+    this.selectedMetricsPageIndex = 0;
+  }
+}
+
+  drop(event: CdkDragDrop<MetricItem[]>, sourceTableOffset: number, destinationTableOffset: number) {
     if (event.previousContainer === event.container) {
-      moveItemInArray(event.container.data, event.previousIndex, event.currentIndex);
+      moveItemInArray(event.container.data, event.previousIndex + destinationTableOffset, event.currentIndex + destinationTableOffset);
     } else {
       const newMetric: MetricItem = event.previousContainer.data[event.previousIndex];
       for (const metric of this.selectedMetrics) {
         metric.checkConflict(newMetric);
       }
+      console.log(sourceTableOffset, destinationTableOffset);
       this.selectedLabelsSet.add(JSON.stringify(Array.from(newMetric.labels.entries())));
       transferArrayItem(
         event.previousContainer.data,
         event.container.data,
-        event.previousIndex,
-        event.currentIndex,
+        event.previousIndex + sourceTableOffset,
+        event.currentIndex + destinationTableOffset,
       );
     }
   }
@@ -105,7 +122,7 @@ export class HephaestusTableComponent implements OnInit {
 
   addQuery() {
     const map = new Map(this.dataProvider.getFilters());
-    if (map.size === 0) {
+    if (map.size === 0){
       return;
     }
     const newMetric = new MetricItem(map, true);
@@ -114,7 +131,7 @@ export class HephaestusTableComponent implements OnInit {
     }
     this.selectedLabelsSet.add(JSON.stringify(Array.from(newMetric.labels.entries())));
     this.selectedMetrics.unshift(newMetric);
-    this.leftScrollBar.scrollTo({ top: 0 });
+    this.leftScrollBar.scrollTo({top: 0});
   }
 
   private getMetrics() {
@@ -126,10 +143,14 @@ export class HephaestusTableComponent implements OnInit {
 
   saveMetrics() {
     //todo
-    const metricsArray = this.selectedMetrics.map((metric) => {
-      return new Filters(metric.labels, metric.isQuery);
-    })
+    let metricsArray: string[][] = [];
+    console.log(this.selectedMetrics);
+    this.selectedMetrics.forEach(metric => {
+      let arr = Array.from((metric.labels.entries())).map(pair => pair[0] + ': ' + pair[1]);
+      metricsArray.push(arr);
+    });
     this.hephaestusService.saveMetrics(metricsArray);
+    this.metricsAdapterService.runRules(metricsArray);
   }
 
 }
